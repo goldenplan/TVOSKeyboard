535b820eda0726bd5ce8798b7130dd25b3414005
Arkushenko Yaroslav <>
2021-09-10 15:51:19 +0300

    Fixed incorrect displaying custom symbols

--- Sources/TVOSKeyboard/KeyboardView.swift
+++ Sources/TVOSKeyboard/KeyboardView.swift
@@ -471,17 +471,19 @@ public class KeyboardView: UIView  {
             button.tag = 3
             
             button.contentEdgeInsets = UIEdgeInsets(top: 0,
-                                                    left: 0,
+                                                    left: 10,
                                                     bottom: 0,
-                                                    right: 0)
+                                                    right: 10)
 
             keyboardButtonsStack.addArrangedSubview(button)
-            
+            let realWidth = (((button.titleLabel?.text ?? "") as NSString).size(withAttributes: [.font: config.keyboardFont]).width)
+            let buttonWidth = max(realWidth, config.keyboardButtonsWidth)
             constrain(button) {
                 button in
-                button.width == config.keyboardButtonsWidth
+                button.width >= buttonWidth
                 
             }
+            button.titleLabel?.sizeToFit()
         }
         
         keyboardButtons.append(rowButtons)
